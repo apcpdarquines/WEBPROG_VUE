39d26bb22dbb149c2ebc4e401d78877a3369fe80
apcpdarquines <pdarquines@student.apc.edu.ph>
2025-01-13 19:03:54 +0800

    Update images.js

--- home/js/images.js
+++ home/js/images.js
@@ -2,7 +2,7 @@ const app2 = Vue.createApp({
    data() {
     return {
       manyFoods: [
-        {name: 'Burrito', url: 'https://www.w3schools.com/vue/img_burrito.svg'},
+        {name: 'Picture 1', url: 'home/Pic1 (1).png'},
         {name: 'Salad', url: 'https://www.w3schools.com/vue/img_salad.svg'},
         {name: 'Cake', url: 'https://www.w3schools.com/vue/img_cake.svg'},
         {name: 'Soup', url: 'https://www.w3schools.com/vue/img_soup.svg'},
